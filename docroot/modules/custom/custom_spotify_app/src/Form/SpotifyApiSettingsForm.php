3d89cfbb88588abd55861a743ba805e9581175d7
Diego Gutierrez <diegosgutierrez@outlook.com>
2023-03-06 05:11:48 -0500

    Cambio variables en el form de setup y agrego servicio

--- docroot/modules/custom/custom_spotify_app/src/Form/SpotifyApiSettingsForm.php
+++ docroot/modules/custom/custom_spotify_app/src/Form/SpotifyApiSettingsForm.php
@@ -4,6 +4,8 @@ namespace Drupal\custom_spotify_app\Form;
 
 use Drupal\Core\Form\ConfigFormBase;
 use Drupal\Core\Form\FormStateInterface;
+use Drupal\Core\Config\ConfigFactoryInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 use SpotifyWebAPI\Session;
 use SpotifyWebAPI\SpotifyWebAPI;
 
@@ -13,6 +15,41 @@ use SpotifyWebAPI\SpotifyWebAPI;
  * @package Drupal\custom_spotify_app\Form
  */
 class SpotifyApiSettingsForm extends ConfigFormBase {
+
+
+  /**
+   * Config settings.
+   *
+   * @var string
+   */
+  const SETTINGS = 'custom_spotify_app.settings';
+
+
+  /**
+   * Class Constructor
+   */
+
+  public function __construct(ConfigFactoryInterface $config_factory){
+    parent::__construct($config_factory);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container) {
+    return new static(
+      $container->get('config.factory')
+    );
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function getEditableConfigNames() {
+    return [
+      static::SETTINGS,
+    ];
+  }
 
   /**
    * {@inheritdoc}
@@ -24,16 +61,9 @@ class SpotifyApiSettingsForm extends ConfigFormBase {
   /**
    * {@inheritdoc}
    */
-  protected function getEditableConfigNames() {
-    return ['custom_spotify_app.settings'];
-  }
-
-  /**
-   * {@inheritdoc}
-   */
   public function buildForm(array $form, FormStateInterface $form_state) {
     // Get the current configuration values.
-    $config = $this->config('custom_spotify_app.settings');
+    $config = $this->config(static::SETTINGS);
 
     // Add a fieldset to the form for the Spotify API credentials.
     $form['spotify_api_credentials'] = [
@@ -93,16 +123,18 @@ class SpotifyApiSettingsForm extends ConfigFormBase {
 
     // Add the Spotify API Query Limit field.
     $form['spotify_api_limits']['spotify_api_query_limit'] = [
-      '#type' => 'textfield',
+      '#type' => 'number',
       '#title' => $this->t('API Query Limit'),
       '#description' => $this->t('Your Spotify API Query Limit.'),
       '#default_value' => $config->get('spotify_api_query_limit'),
+      '#min' => 0,
+      '#max' => 20,
       '#required' => FALSE,
     ];
 
     // Add the Spotify API Query Offset field.
     $form['spotify_api_limits']['spotify_api_query_offset'] = [
-      '#type' => 'textfield',
+      '#type' => 'number',
       '#title' => $this->t('API Query Offset'),
       '#description' => $this->t('Your Spotify API Query Offset.'),
       '#default_value' => $config->get('spotify_api_query_offset'),
@@ -125,25 +157,56 @@ class SpotifyApiSettingsForm extends ConfigFormBase {
     ];
 
     // Add a submit button to save the form values.
-    $form['submit'] = [
-      '#type' => 'submit',
-      '#value' => $this->t('Save'),
-    ];
-
-    // Add a submit button to save the form values.
-    $form['import'] = [
+    $form['actions']['import'] = [
       '#type' => 'submit',
       '#value' => $this->t('Import New Content'),
       '#submit' => array('::crawl_new_info'),
+      '#weight' => 10,
     ];
 
-    return $form;
+    return parent::buildForm($form, $form_state);
+
+  }
+
+
+  /**
+   * {@inheritdoc}
+   */
+  public function submitForm(array &$form, FormStateInterface $form_state) {
+    // Retrieve the configuration.
+    $this->config(static::SETTINGS)
+      ->set('spotify_access_token', $form_state->getValue('spotify_access_token'))
+      ->set('spotify_client_id', $form_state->getValue('spotify_client_id'))
+      ->set('spotify_client_secret', $form_state->getValue('spotify_client_secret'))
+      ->set('spotify_access_token', $form_state->getValue('spotify_access_token'))
+      ->set('spotify_access_token_created_at', $form_state->getValue('spotify_access_token_created_at'))
+      ->set('spotify_access_token_expiration_time', $form_state->getValue('spotify_access_token_expiration_time'))
+      ->set('spotify_api_query_limit', $form_state->getValue('spotify_api_query_limit'))
+      ->set('spotify_api_query_offset', $form_state->getValue('spotify_api_query_offset'))
+      ->set('caching_time', $form_state->getValue('caching_time'))
+      ->save();
+    parent::submitForm($form, $form_state);
+  }
+
+
+  public function validateForm(array &$form, FormStateInterface $form_state) {
+    parent::validateForm($form, $form_state);
+
+    if(!$form_state->getValue('spotify_access_token')){
+      $serv = \Drupal::service('custom_spotify_app.api_service');
+      $serv->auth();
+    }
+
   }
 
     /**
    * Custom function to scrap and update artist, album and song information.
    */
   public function crawl_new_info(array &$form, FormStateInterface $form_state) {
+
+
+    $config = $this->config('custom_spotify_app.settings');
+
     // Retrieve the API credentials from the module settings.
     $client_id = \Drupal::config('custom_spotify_app.settings')->get('spotify_client_id');
     $client_secret = \Drupal::config('custom_spotify_app.settings')->get('spotify_client_secret');
@@ -151,13 +214,18 @@ class SpotifyApiSettingsForm extends ConfigFormBase {
     // Initialize the Spotify API client.
     $api = new \SpotifyWebAPI\SpotifyWebAPI();
     $session = new \SpotifyWebAPI\Session($client_id, $client_secret);
-    $api->setAccessToken($session->getAccessToken());
+    $session->requestCredentialsToken();
+    $accessToken = $session->getAccessToken();
+
+
+
+    //$api->setAccessToken($session->getAccessToken());
 
     // Define the number of artists to retrieve.
-    $artist_limit = \Drupal::config('custom_spotify_app.settings')->get('spotify_api_query_limit');
+    $query_limit = \Drupal::config('custom_spotify_app.settings')->get('spotify_api_query_limit');
 
     // Retrieve a list of artists from the Spotify API.
-    $artists = $api->search('artist', 'spotify', ['limit' => $artist_limit])->artists->items;
+    $artists = $api->search('artist', 'spotify', ['limit' => $query_limit])->artists->items;
 
     // Loop through each artist and create an "Artist" node.
     foreach ($artists as $artist) {
